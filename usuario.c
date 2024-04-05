3d22bbac0327385d69caa1473982c5ecb183f185
Carlos Henrique <114544383+CarlosH6@users.noreply.github.com>
2024-04-04 21:37:37 -0300

    novas funcoes implementadas

--- usuario.c
+++ usuario.c
@@ -4,102 +4,167 @@
 #include <stdlib.h>
 #include <string.h>
 
-void adicionarUsuario(Usuario *usuarios, int *numUsuarios) {
+void adicionarUsuario(Usuario **cabecaUsuarios) {
   printf("\033[2J\033[H");
   printf("Adicionar Usuário\n");
+
+  Usuario *novoUsuario = (Usuario *)malloc(sizeof(Usuario));
+
+  if (novoUsuario == NULL) {
+    printf("Erro ao alocar memória para novo usuário\n");
+    return;
+  }
+
   printf("Digite o nome do usuário: ");
-  scanf("%s", usuarios[*numUsuarios].nome);
+  scanf(" %[^\n]", novoUsuario->nome);
+  getchar();
+
   printf("Digite o contato do usuário: ");
-  scanf("%d", &usuarios[*numUsuarios].contato);
-  (*numUsuarios)++;
+  scanf("%d", &novoUsuario->contato);
+
+  novoUsuario->prox = *cabecaUsuarios;
+  *cabecaUsuarios = novoUsuario;
+
   printf("Usuário adicionado!\n\n");
 }
 
-void removerUsuario(Usuario *usuarios, int *numUsuarios) {
+void removerUsuario(Usuario **cabecaUsuarios) {
   char nome[50];
   printf("\033[2J\033[H");
   printf("Remover Usuário\n");
   printf("Digite o nome do usuário que deseja remover: ");
-  scanf("%s", nome);
-  for (int i = 0; i < *numUsuarios; i++) {
-    if (strcmp(usuarios[i].nome, nome) == 0) {
-      strcpy(usuarios[i].nome, "");
-      numUsuarios--;
+  scanf(" %[^\n]", nome);
+
+  Usuario *atual = *cabecaUsuarios;
+  Usuario *anterior = NULL;
+
+  while (atual != NULL) {
+    if (strcmp(atual->nome, nome) == 0) {
+      if (anterior != NULL) {
+        anterior->prox = atual->prox;
+      } else {
+        *cabecaUsuarios = atual->prox;
+      }
+      free(atual);
+      printf("Usuário removido com sucesso.\n");
+      return;
     }
+    anterior = atual;
+    atual = atual->prox;
   }
+  printf("Usuário não encontrado.\n");
 }
 
-void copiarDados(Usuario *usuarios, int *numUsuarios, Livro *livros,
-                 int *numLivros) {
-  const char *dadosUsuarios = "usuarios.txt";
-  const char *dadosLivros = "livros.txt";
+void listarUsuarios(Usuario *cabecaUsuarios) {
+  printf("\033[2J\033[H");
+  Usuario *atual = cabecaUsuarios;
+  int i = 1;
 
-  FILE *Usuarios = fopen(dadosUsuarios, "r");
-  FILE *Livros = fopen(dadosLivros, "r");
+  while (atual != NULL) {
+    printf("Usuário %d: %s, %d\n", i++, atual->nome, atual->contato);
+    atual = atual->prox;
+  }
+  printf("\n");
+}
 
-  if (Usuarios == NULL || Livros == NULL) {
+void escreverDados(Usuario *cabecaUsuarios, Livro *cabecaLivros) {
+  FILE *arquivoUsuarios = fopen("usuarios.txt", "w");
+  FILE *arquivoLivros = fopen("livros.txt", "w");
+
+  if (arquivoUsuarios == NULL || arquivoLivros == NULL) {
     printf("Erro ao abrir os arquivos\n");
     return;
   }
 
-  *numUsuarios = 0;
-  *numLivros = 0;
-
-  while (*numUsuarios < 100 &&
-         fscanf(Usuarios, "%s - %d - %s", usuarios[*numUsuarios].nome,
-                &usuarios[*numUsuarios].contato,
-                usuarios[*numUsuarios].livrosEmprestados) == 3) {
-    (*numUsuarios)++;
+  Usuario *atualUsuario = cabecaUsuarios;
+  while (atualUsuario != NULL) {
+    if (strcmp(atualUsuario->livrosEmprestados, "") == 0) {
+      fprintf(arquivoUsuarios, "%s\t%d\nNenhum\n", atualUsuario->nome,
+              atualUsuario->contato);
+    } else {
+      fprintf(arquivoUsuarios, "%s\t%d\n%s\n", atualUsuario->nome,
+              atualUsuario->contato, atualUsuario->livrosEmprestados);
+    }
+    atualUsuario = atualUsuario->prox;
   }
 
-  while (*numLivros < 100 &&
-         fscanf(Livros, "%s - %s - %s - %d\n", livros[*numLivros].titulo,
-                livros[*numLivros].autor, livros[*numLivros].editora,
-                &livros[*numLivros].anoPublicacao) == 4) {
-    (*numLivros)++;
+  Livro *atualLivro = cabecaLivros;
+  while (atualLivro != NULL) {
+    fprintf(arquivoLivros, "%s\t%s\t%s\t%d\n", atualLivro->titulo,
+            atualLivro->autor, atualLivro->editora, atualLivro->anoPublicacao);
+    atualLivro = atualLivro->prox;
   }
 
-  fclose(Usuarios);
-  fclose(Livros);
+  fclose(arquivoUsuarios);
+  fclose(arquivoLivros);
 }
 
-void escreverDados(Usuario *usuarios, int *numUsuarios, Livro *livros,
-                   int *numLivros) {
-  const char *dadosUsuarios = "usuarios.txt";
-  const char *dadosLivros = "livros.txt";
+void copiarDados(Usuario **cabecaUsuarios, Livro **cabecaLivros) {
+  FILE *arquivoUsuarios = fopen("usuarios.txt", "r");
+  FILE *arquivoLivros = fopen("livros.txt", "r");
 
-  FILE *Usuarios = fopen(dadosUsuarios, "w");
-  FILE *Livros = fopen(dadosLivros, "w");
-
-  if (Usuarios == NULL || Livros == NULL) {
-    printf("Erro");
+  if (arquivoUsuarios == NULL || arquivoLivros == NULL) {
+    printf("Erro ao abrir os arquivos\n");
     return;
   }
 
-  for (int i = 0; i < *numUsuarios; i++) {
-    if (strcmp(usuarios[i].livrosEmprestados, "") == 0) {
-      fprintf(Usuarios, "%s - %d - Nenhum\n", usuarios[i].nome,
-              usuarios[i].contato);
-    } else if (strcmp(usuarios[i].nome, "") == 0) {
+  char tempNome[50];
+  int tempContato;
+  char tempLivrosEmprestados[100];
 
-    } else {
-      fprintf(Usuarios, "%s - %d - %s\n", usuarios[i].nome, usuarios[i].contato,
-              usuarios[i].livrosEmprestados);
+  while (fscanf(arquivoUsuarios, " %[^\t] \t %d \n %[^\n]", tempNome,
+                &tempContato, tempLivrosEmprestados) == 3) {
+    Usuario *novoUsuario = (Usuario *)malloc(sizeof(Usuario));
+    if (novoUsuario == NULL) {
+      printf("Erro ao alocar memória para novo usuário\n");
+      return;
     }
+    strcpy(novoUsuario->nome, tempNome);
+    novoUsuario->contato = tempContato;
+    strcpy(novoUsuario->livrosEmprestados, tempLivrosEmprestados);
+    novoUsuario->prox = *cabecaUsuarios;
+    *cabecaUsuarios = novoUsuario;
   }
 
-  for (int i = 0; i < *numLivros; i++) {
-    fprintf(Livros, "%s - %s - %s - %d\n", livros[i].titulo, livros[i].autor,
-            livros[i].editora, livros[i].anoPublicacao);
-  }
+  char tempTitulo[50];
+  char tempAutor[50];
+  char tempEditora[50];
+  int tempAnoPublicacao;
 
-  fclose(Usuarios);
-  fclose(Livros);
+  while (fscanf(arquivoLivros, "%[^\t] \t %[^\t] \t %[^\t] \t %d\n",
+                  tempTitulo, tempAutor, tempEditora, &tempAnoPublicacao) == 4) {
+      Livro *novoLivro = (Livro *)malloc(sizeof(Livro));
+      if (novoLivro == NULL) {
+        printf("Erro ao alocar memória\n");
+        return;
+      }
+      strcpy(novoLivro->titulo, tempTitulo);
+      strcpy(novoLivro->autor, tempAutor);
+      strcpy(novoLivro->editora, tempEditora);
+      novoLivro->anoPublicacao = tempAnoPublicacao;
+      novoLivro->prox = *cabecaLivros;
+      *cabecaLivros = novoLivro;
+  }
 }
 
-void Sair(Usuario *usuarios, int *numUsuarios, Livro *livros, int *numLivros) {
-  adicionarListaEmOrdemAlfabetica(usuarios, *numUsuarios, livros, *numLivros);
-  escreverDados(usuarios, numUsuarios, livros, numLivros);
-  printf("Saindo...");
+void Sair(Usuario *cabecaUsuarios, Livro *cabecaLivros) {
+  adicionarListaEmOrdemAlfabetica(&cabecaUsuarios);
+  escreverDados(cabecaUsuarios, cabecaLivros);
+  Usuario *atualUsuario = cabecaUsuarios;
+
+  while (atualUsuario != NULL) {
+    Usuario *temp = atualUsuario;
+    atualUsuario = atualUsuario->prox;
+    free(temp);
+  }
+
+  Livro *atualLivro = cabecaLivros;
+  while (atualLivro != NULL) {
+    Livro *temp = atualLivro;
+    atualLivro = atualLivro->prox;
+    free(temp);
+  }
+
+  printf("\nSaindo...");
   return;
 }
