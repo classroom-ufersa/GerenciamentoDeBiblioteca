7b87913107d5376d4037140896e07f769312ba38
Carlos Henrique <114544383+CarlosH6@users.noreply.github.com>
2024-04-04 21:38:10 -0300

    agora tem lista encadeada

--- usuario.h
+++ usuario.h
@@ -5,28 +5,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct livro Livro;
+struct livro;
 
-typedef struct usuario Usuario;
-
-struct usuario {
+typedef struct usuario {
   char nome[50];
   int contato;
   char livrosEmprestados[50];
-};
+  struct usuario *prox;
+} Usuario;
 
-void adicionarUsuario(Usuario *usuarios, int *numUsuarios);
-
-void removerUsuario(Usuario *usuarios, int *numUsuarios);
-
-void listarUsuarios(Usuario *usuarios, int *numUsuarios);
-
-void escreverDados(Usuario *usuarios, int *numUsuarios, Livro *livros,
-                   int *numLivros);
-
-void copiarDados(Usuario *usuarios, int *numUsuarios, Livro *livros,
-                 int *numLivros);
-
-void Sair(Usuario *usuarios, int *numUsuarios, Livro *livros, int *numLivros);
+void adicionarUsuario(Usuario **cabecaUsuarios);
+void removerUsuario(Usuario **cabecaUsuarios);
+void listarUsuarios(Usuario *cabecaUsuarios);
+void escreverDados(Usuario *cabecaUsuarios, struct livro *cabecaLivros);
+void copiarDados(Usuario **cabecaUsuarios, struct livro **cabecaLivros);
+void Sair(Usuario *cabecaUsuarios, struct livro *cabecaLivros);
 
 #endif
