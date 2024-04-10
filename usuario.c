de05d8696b0576e06b4f36754b10228d7e8c85f7
Carlos Henrique <114544383+CarlosH6@users.noreply.github.com>
2024-04-09 23:06:00 -0300

    agora tem funcao "verificacao"

--- usuario.c
+++ usuario.c
@@ -1,39 +1,63 @@
 #include "usuario.h"
 #include "livro.h"
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 void adicionarUsuario(Usuario **cabecaUsuarios) {
   printf("\033[2J\033[H");
-  printf("Adicionar Usuário\n");
+  printf("| Adicionar Usuário\n");
 
   Usuario *novoUsuario = (Usuario *)malloc(sizeof(Usuario));
 
   if (novoUsuario == NULL) {
-    printf("Erro ao alocar memória para novo usuário\n");
+    printf("Erro ao alocar memória\n");
     return;
   }
 
-  printf("Digite o nome do usuário: ");
+  printf("| Digite o nome do usuario:\n| -> ");
   scanf(" %[^\n]", novoUsuario->nome);
-  getchar();
+  if (verificar(novoUsuario->nome, 0)) {
+    free(novoUsuario);
+    return;
+  }
 
-  printf("Digite o contato do usuário: ");
-  scanf("%d", &novoUsuario->contato);
+  printf("| Digite o contato do usuario:\n| -> ");
+  scanf(" %[^\n]", novoUsuario->contato);
+
+  if (verificar(novoUsuario->contato, 1)) {
+    free(novoUsuario);
+    return;
+  }
+
+  Usuario *atual = *cabecaUsuarios;
+
+  while (atual != NULL) {
+    if (strcmp(atual->nome, novoUsuario->nome) == 0 ||
+        strcmp(atual->contato, novoUsuario->contato) == 0) {
+      printf("| Nome ou contato já existente!\n|\n");
+      free(novoUsuario);
+      return;
+    }
+    atual = atual->prox;
+  }
 
   novoUsuario->prox = *cabecaUsuarios;
   *cabecaUsuarios = novoUsuario;
 
-  printf("Usuário adicionado!\n\n");
+  printf("| Usuario adicionado!\n|\n");
 }
 
 void removerUsuario(Usuario **cabecaUsuarios) {
-  char nome[50];
+  char nome[500];
   printf("\033[2J\033[H");
-  printf("Remover Usuário\n");
-  printf("Digite o nome do usuário que deseja remover: ");
+  printf("| Remover Usuario\n");
+  printf("| Digite o nome do usuario que deseja remover:\n| -> ");
   scanf(" %[^\n]", nome);
+  if (verificar(nome, 0)) {
+    return;
+  }
 
   Usuario *atual = *cabecaUsuarios;
   Usuario *anterior = NULL;
@@ -46,13 +70,13 @@ void removerUsuario(Usuario **cabecaUsuarios) {
         *cabecaUsuarios = atual->prox;
       }
       free(atual);
-      printf("Usuário removido com sucesso.\n");
+      printf("| Usuário removido com sucesso.\n|\n");
       return;
     }
     anterior = atual;
     atual = atual->prox;
   }
-  printf("Usuário não encontrado.\n");
+  printf("| Usuário não encontrado.\n|\n");
 }
 
 void listarUsuarios(Usuario *cabecaUsuarios) {
@@ -60,11 +84,16 @@ void listarUsuarios(Usuario *cabecaUsuarios) {
   Usuario *atual = cabecaUsuarios;
   int i = 1;
 
+  if (atual == NULL) {
+    printf("|\n| Nenhum usuário cadastrado.\n|\n");
+    return;
+  }
+
   while (atual != NULL) {
-    printf("Usuário %d: %s, %d\n", i++, atual->nome, atual->contato);
+    printf("| Usuário %d: %s | %s\n", i++, atual->nome, atual->contato);
     atual = atual->prox;
   }
-  printf("\n");
+  printf("|\n");
 }
 
 void escreverDados(Usuario *cabecaUsuarios, Livro *cabecaLivros) {
@@ -72,17 +101,17 @@ void escreverDados(Usuario *cabecaUsuarios, Livro *cabecaLivros) {
   FILE *arquivoLivros = fopen("livros.txt", "w");
 
   if (arquivoUsuarios == NULL || arquivoLivros == NULL) {
-    printf("Erro ao abrir os arquivos\n");
+    printf("| Erro ao abrir os arquivos\n|\n");
     return;
   }
 
   Usuario *atualUsuario = cabecaUsuarios;
   while (atualUsuario != NULL) {
-    if (strcmp(atualUsuario->livrosEmprestados, "") == 0) {
-      fprintf(arquivoUsuarios, "%s\t%d\nNenhum\n", atualUsuario->nome,
+    if (strcmp(atualUsuario->livrosEmprestados, "|") == 0) {
+      fprintf(arquivoUsuarios, "%s\t%s\n|NENHUM\n", atualUsuario->nome,
               atualUsuario->contato);
     } else {
-      fprintf(arquivoUsuarios, "%s\t%d\n%s\n", atualUsuario->nome,
+      fprintf(arquivoUsuarios, "%s\t%s\n%s\n", atualUsuario->nome,
               atualUsuario->contato, atualUsuario->livrosEmprestados);
     }
     atualUsuario = atualUsuario->prox;
@@ -90,8 +119,11 @@ void escreverDados(Usuario *cabecaUsuarios, Livro *cabecaLivros) {
 
   Livro *atualLivro = cabecaLivros;
   while (atualLivro != NULL) {
-    fprintf(arquivoLivros, "%s\t%s\t%s\t%d\n", atualLivro->titulo,
-            atualLivro->autor, atualLivro->editora, atualLivro->anoPublicacao);
+    if (atualLivro->copias != 0) {
+      fprintf(arquivoLivros, "%s\t%s\t%s\t%d\t%d\n", atualLivro->titulo,
+              atualLivro->autor, atualLivro->editora, atualLivro->anoPublicacao,
+              atualLivro->copias);
+    }
     atualLivro = atualLivro->prox;
   }
 
@@ -104,51 +136,57 @@ void copiarDados(Usuario **cabecaUsuarios, Livro **cabecaLivros) {
   FILE *arquivoLivros = fopen("livros.txt", "r");
 
   if (arquivoUsuarios == NULL || arquivoLivros == NULL) {
-    printf("Erro ao abrir os arquivos\n");
+    printf("| Erro ao abrir os arquivos\n|\n");
     return;
   }
 
-  char tempNome[50];
-  int tempContato;
-  char tempLivrosEmprestados[100];
+  char tempNome[500];
+  char tempContato[500];
+  char tempLivrosEmprestados[500];
 
-  while (fscanf(arquivoUsuarios, " %[^\t] \t %d \n %[^\n]", tempNome,
-                &tempContato, tempLivrosEmprestados) == 3) {
+  while (fscanf(arquivoUsuarios, " %[^\t] \t %[^\n] \n %[^\n]", tempNome,
+                tempContato, tempLivrosEmprestados) == 3) {
     Usuario *novoUsuario = (Usuario *)malloc(sizeof(Usuario));
     if (novoUsuario == NULL) {
-      printf("Erro ao alocar memória para novo usuário\n");
+      printf("Erro ao alocar memória\n");
       return;
     }
     strcpy(novoUsuario->nome, tempNome);
-    novoUsuario->contato = tempContato;
+    strcpy(novoUsuario->contato, tempContato);
     strcpy(novoUsuario->livrosEmprestados, tempLivrosEmprestados);
     novoUsuario->prox = *cabecaUsuarios;
     *cabecaUsuarios = novoUsuario;
   }
 
-  char tempTitulo[50];
-  char tempAutor[50];
-  char tempEditora[50];
+  char tempTitulo[500];
+  char tempAutor[500];
+  char tempEditora[500];
   int tempAnoPublicacao;
+  int tempCopias;
 
-  while (fscanf(arquivoLivros, "%[^\t] \t %[^\t] \t %[^\t] \t %d\n",
-                  tempTitulo, tempAutor, tempEditora, &tempAnoPublicacao) == 4) {
-      Livro *novoLivro = (Livro *)malloc(sizeof(Livro));
-      if (novoLivro == NULL) {
-        printf("Erro ao alocar memória\n");
-        return;
-      }
-      strcpy(novoLivro->titulo, tempTitulo);
-      strcpy(novoLivro->autor, tempAutor);
-      strcpy(novoLivro->editora, tempEditora);
-      novoLivro->anoPublicacao = tempAnoPublicacao;
-      novoLivro->prox = *cabecaLivros;
-      *cabecaLivros = novoLivro;
+  while (fscanf(arquivoLivros, "%[^\t] \t %[^\t] \t %[^\t] \t %d \t %d\n",
+                tempTitulo, tempAutor, tempEditora, &tempAnoPublicacao,
+                &tempCopias) == 5) {
+    Livro *novoLivro = (Livro *)malloc(sizeof(Livro));
+    if (novoLivro == NULL) {
+      printf("Erro ao alocar memória\n");
+      return;
+    }
+    strcpy(novoLivro->titulo, tempTitulo);
+    strcpy(novoLivro->autor, tempAutor);
+    strcpy(novoLivro->editora, tempEditora);
+    novoLivro->anoPublicacao = tempAnoPublicacao;
+    novoLivro->copias = tempCopias;
+    novoLivro->prox = *cabecaLivros;
+    *cabecaLivros = novoLivro;
   }
+
+  fclose(arquivoUsuarios);
+  fclose(arquivoLivros);
 }
 
 void Sair(Usuario *cabecaUsuarios, Livro *cabecaLivros) {
-  adicionarListaEmOrdemAlfabetica(&cabecaUsuarios);
+  ordenarAlfabeticamente(&cabecaUsuarios);
   escreverDados(cabecaUsuarios, cabecaLivros);
   Usuario *atualUsuario = cabecaUsuarios;
 
@@ -165,6 +203,28 @@ void Sair(Usuario *cabecaUsuarios, Livro *cabecaLivros) {
     free(temp);
   }
 
-  printf("\nSaindo...");
+  printf("|\n| Saindo...");
   return;
+}
+
+int verificar(char *conteudo, int numeroOuLetra) {
+
+  for (int i = 0; conteudo[i] != '\0'; i++) {
+    conteudo[i] = toupper(conteudo[i]);
+  }
+  for (int i = 0; conteudo[i] != '\0'; i++) {
+    if (numeroOuLetra == 1) {
+      if (!isdigit(conteudo[i]) && conteudo[i] != ' ') {
+        printf("| Entrada inválida!\n| Apenas números são permitidos.\n|\n");
+        return 1;
+      }
+    } else if (numeroOuLetra == 0) {
+      if (!isalpha(conteudo[i]) && conteudo[i] != ' ') {
+        printf("| Entrada inválida!\n| Apenas letras são permitidas.\n|\n");
+        return 1;
+      }
+    }
+  }
+
+  return 0;
 }
