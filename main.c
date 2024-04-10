7e2802820b06d2bed8176987b6553f855a54c8f5
Carlos Henrique <114544383+CarlosH6@users.noreply.github.com>
2024-04-10 08:31:16 -0300

    anti-vulnerabilidades

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "livro.h"
 #include "usuario.h"
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,53 +8,57 @@ int main(void) {
 
   Usuario *cabecaUsuarios = NULL;
   Livro *cabecaLivros = NULL;
-  int opcao;
+  char opcao[10];
 
   copiarDados(&cabecaUsuarios, &cabecaLivros);
 
   while (1) {
-    printf("Seja bem vindo a biblioteca\n");
-    printf("Escolha a ação que deseja realizar:\n");
-    printf("1 - Adicionar usuário\n");
-    printf("2 - Remover usuário\n");
-    printf("3 - Emprestar livro\n");
-    printf("4 - Devolver livro\n");
-    printf("5 - Editar informações do livro\n");
-    printf("6 - Buscar Livro por nome\n");
-    printf("7 - Listar usuários\n");
-    printf("8 - Sair\n");
-   // printf("9 - Adicionar livro"); Desenvolvendo
-    scanf("%d", &opcao);
-    switch (opcao) {
-    case 1:
-      adicionarUsuario(&cabecaUsuarios); // Funcionando
-      break;
-    case 2:
-      removerUsuario(&cabecaUsuarios); // Funcionando
-      break;
-    case 3:
-      emprestarLivro(cabecaUsuarios, cabecaLivros); // Funcionando parcialmente
-      break;
-    case 4:
-      devolverLivro(cabecaUsuarios, cabecaLivros); // Funcionando parcialmente 
-      break;
-    case 5:
-      editarLivro(cabecaLivros); // Funcionando
-      break;
-    case 6:
-      buscarLivro(cabecaLivros); // Funcionando
-      break;
-    case 7:
-      listarUsuarios(cabecaUsuarios); // Funcionando
-      break;
-    case 8:
-      Sair(cabecaUsuarios, cabecaLivros); // Funcionando
-      return 0;
-      break;
-    // case 9:
-      // adicionarLivro(Livro **cabecaLivros); // Desenvolvendo (por que essa função?)
-    default:
-      printf("Opção inválida, tente novamente.\n\n");
+    printf("| -=-=-=-=- Biblioteca Bytes -=-=-=-=-\n"
+           "|\n"
+           "| Escolha a acao que deseja realizar:\n"
+           "| [1] - Adicionar usuario\n"
+           "| [2] - Remover usuario\n"
+           "| [3] - Emprestar livro\n"
+           "| [4] - Devolver livro\n"
+           "| [5] - Editar informacoes do livro\n"
+           "| [6] - Buscar livro\n"
+           "| [7] - Listar usuarios\n"
+           "| [8] - Sair\n| -> ");
+    scanf("%s", opcao);
+
+    if (verificar(opcao, 1) == 0) {
+      switch (opcao[0]) {
+      case '1':
+        adicionarUsuario(&cabecaUsuarios);
+        break;
+      case '2':
+        removerUsuario(&cabecaUsuarios);
+        break;
+      case '3':
+        emprestarLivro(cabecaUsuarios, &cabecaLivros);
+        break;
+      case '4':
+        devolverLivro(cabecaUsuarios, &cabecaLivros);
+        break;
+      case '5':
+        editarLivro(cabecaUsuarios, &cabecaLivros);
+        break;
+      case '6':
+        buscarLivro(cabecaUsuarios, &cabecaLivros);
+        break;
+      case '7':
+        listarUsuarios(cabecaUsuarios);
+        break;
+      case '8':
+        Sair(cabecaUsuarios, cabecaLivros);
+        return 0;
+      default:
+        printf("\033[2J\033[H");
+        printf("| Opcao invalida, tente novamente.\n\n");
+      }
+    } else {
+      printf("\033[2J\033[H");
+      printf("| Opcao invalida, tente novamente.\n\n");
     }
   }
   return 0;
