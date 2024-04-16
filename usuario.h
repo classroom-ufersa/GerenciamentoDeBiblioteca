5a90cf2acb9933a990e8fc13e3f44c77e7ea3bf5
Carlos Henrique <114544383+CarlosH6@users.noreply.github.com>
2024-04-16 17:56:59 -0300

    hotfix

--- usuario.h
+++ usuario.h
@@ -8,9 +8,9 @@
 struct livro;
 
 typedef struct usuario {
-  char nome[500];
-  char contato[500];
-  char livrosEmprestados[500];
+  char nome[200];
+  char contato[200];
+  char livrosEmprestados[200];
   struct usuario *prox;
 } Usuario;
 
@@ -24,10 +24,10 @@ void escreverDados(Usuario *cabecaUsuarios, struct livro *cabecaLivros);
 
 void copiarDados(Usuario **cabecaUsuarios, struct livro **cabecaLivros);
 
-int verificar(char conteudo[], int numeroOuLetra);
+int verificar(char *conteudo, int numeroOuLetra);
 
 void Sair(Usuario *cabecaUsuarios, struct livro *cabecaLivros);
 
-int conferirCaractere(char conteudo[]);
+int conferirCaractere(char *conteudo);
 
 #endif
